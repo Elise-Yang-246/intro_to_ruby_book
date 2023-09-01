54cfebe4d1499df1ac09fa6013a6416a238f1ca3
Elise-Yang-246 <elise.codes.daily@gmail.com>
2023-09-01 18:45:31 +0000

    Chapter 5: Methods - Following along with chapter examples

---
+++ chp5_methods/return.rb
@@ -0,0 +1,11 @@
+def add_three(number)
+  puts 'test 1'
+  puts 'test 2'
+  return number + 3
+  number + 4
+  puts 'test 3'
+  puts 'test 4'
+end
+
+returned_value = add_three(4)
+puts returned_value
