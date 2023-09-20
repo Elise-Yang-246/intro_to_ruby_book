9ef713ebb4e11121184acc9d29eea54eaaa586b3
Elise-Yang-246 <elise.codes.daily@gmail.com>
2023-09-20 21:44:18 +0000

    Chapter 11: Exercises - Exercise 5

---
+++ ch11_exercises/ch11_ex5.rb
@@ -0,0 +1,15 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Append 11 to the end of the array
+array.append(11)
+
+# Prepend 0 to the beginning of the array
+array.prepend(0)
+
+# Get rid of 11
+array.pop
+
+# Append a 3
+array.append(3)
+
+p array
