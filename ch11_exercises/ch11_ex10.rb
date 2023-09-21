ee4ad2a1b20cdc9d5dc24a23429d3bd20acfe162
Elise-Yang-246 <elise.codes.daily@gmail.com>
2023-09-21 01:29:22 +0000

    Chapter 11: Exercises - Exercise 10

---
+++ ch11_exercises/ch11_ex10.rb
@@ -0,0 +1,7 @@
+# Can hash values be arrays?
+  # --> Yes. For example:
+p hash_with_arrays_as_values = { fruits: ['apples', 'bananas', 'oranges'], vegetables: ['spinach', 'lettuce', 'carrots'] }
+
+# Can you have an array of hashes?
+#   --> Yes. For example:
+p array_of_hashes = [ {k1: 'value 1'}, {k2: 'value 2'}, {k3: 'value 3', k4: 'value 4'}]
